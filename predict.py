7c502c84a5bf2c4dbd44ae8f7954cdc570d6f672
Aaron Ferber <amf272@cornell.edi>
2021-09-19 10:10:16 -0700

    added code pipeline of fetch data, train, and predict

---
+++ predict.py
@@ -0,0 +1,143 @@
+import pickle
+from pathlib import Path
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from model_utils import SaveableModel
+
+FORECAST_LENGTH = 90
+
+HISTORY_LENGTH = 10
+STEP = 1 # for creation of dataset
+
+used_cols = ['PRESENT_STORAGE_TMC', 'INFLOW_CUSECS', 'OUTFLOW_CUECS', 'tempC', 'windspeedKmph', 'precipMM', 'humidity', 'pressure (mB)', 'WindChillC', 'RES_LEVEL_FT', 'OUTFLOW_CUECS_HEMAVATHI', 'OUTFLOW_CUECS_HARANGI']
+
+
+data_dir = Path("data")
+data_file = data_dir / "updated_data_set_full.csv"
+data_dict_file = data_dir / "data_dict.pkl"
+future_data_file = data_dir / "future_dataset.pkl"
+
+
+model_dir = Path("saved_models")
+model_file = model_dir / "model.pkl"
+ub_model_file = model_dir / "ub_model.pkl"
+lb_model_file = model_dir / "lb_model.pkl"
+
+plot_dir = Path("plots")
+
+prediction_dir = Path("predictions")
+prediction_file = prediction_dir / "future_predictions_updated_model_weights_sep-nov.csv"
+
+# load formatted data dictionary
+with open(data_dict_file, "rb") as f:
+    data_dict = pickle.load(f)
+
+# load models
+with open(model_file, "rb") as f:
+    mid_model = pickle.load(f)
+
+with open(ub_model_file, "rb") as f:
+    ub_model = pickle.load(f)
+
+with open(lb_model_file, "rb") as f:
+    lb_model = pickle.load(f)
+
+def create_time_steps(length):
+    return list(range(-length, 0))
+
+#plotting function
+def multi_step_plot(history, true_future, prediction, title=""):
+    plt.figure(figsize=(12, 6))
+    num_in = create_time_steps(len(history))
+    num_out = len(true_future)
+    plt.grid()
+    plt.plot(num_in, np.array(history), label='History')
+    plt.plot(np.arange(num_out)/STEP, np.array(true_future), 'bo',
+            label='True Future')
+    if prediction.any():
+        plt.plot(np.arange(num_out)/STEP, np.array(prediction), 'ro',
+                label='Predicted Future')
+    plt.legend(loc='upper left')
+    plt.title(title)
+    plt.show()
+
+# evaluation
+def generate_plots_for_partition(partition_name):
+    plt.figure(figsize=(20,10))
+    plt.style.use('tableau-colorblind10')
+    xs = data_dict[partition_name]["X"]
+    ys = mid_model.rescale(data_dict[partition_name]["y"])
+    ypred_lb = lb_model.predict(xs)
+    ypred_mid = mid_model.predict(xs)
+    ypred_ub = ub_model.predict(xs)
+
+    num_past = xs.shape[0]
+
+    num_future = ypred_lb.shape[0]
+
+    x_indices_future = data_dict[partition_name]["dates"] #np.arange(num_future)
+    plt.figure(figsize=(20,10))
+    ticks = plt.xticks(fontsize = 20)
+    ticks = plt.yticks(fontsize = 22)
+    # plot future predictions
+    plt.fill_between(x_indices_future, ypred_lb, ypred_ub, color='b', alpha=0.1, label = 'Predicted Lower/Upper Bound')
+    plt.plot(x_indices_future, ypred_mid, "--", label = 'Predicted Value')
+    plt.plot(x_indices_future, ys, label = 'True Value')
+    plt.legend(fontsize = 22)
+    plt.title(f"{partition_name} Evaluation", fontdict = {'fontsize' : 36})
+
+    plt.savefig(plot_dir / f"{partition_name}_results.png", dpi = 800)
+    plt.close()
+
+generate_plots_for_partition("train")
+generate_plots_for_partition("validation")
+generate_plots_for_partition("test")
+
+with open(future_data_file, "rb") as f:
+    future_xs = pickle.load(f)
+
+partition_name = "future"
+
+# d = data_dict[partition_name]
+xs = future_xs[-90:]
+
+ypred_lb = lb_model.predict(xs)
+ypred_mid = mid_model.predict(xs)
+ypred_ub = ub_model.predict(xs)
+
+num_past = xs.shape[0]
+
+num_future = ypred_lb.shape[0]
+
+x_indices_future = np.arange(num_future)
+plt.figure(figsize=(20,10))
+
+# plot future predictions
+plt.fill_between(x_indices_future, ypred_lb, ypred_ub, color='b', alpha=0.1, label = "Predicted Lower/Upper Bound")
+plt.plot(x_indices_future, ypred_mid, "--", label = "Predicted Value")
+# plt.plot(x_indices_future, ys)
+
+plt.title("Future Forecasts", fontdict = {'fontsize' : 26})
+plt.style.use('tableau-colorblind10')
+plt.legend(prop={"size":20})
+plt.grid(True)
+plt.savefig(plot_dir / "future_forecast.png", dpi = 1200)
+plt.close()
+
+data_to_dump = {
+    "mid":ypred_mid,
+    "lb": ypred_lb,
+    "ub":ypred_ub,
+}
+
+pd.DataFrame(data_to_dump).to_csv(prediction_file, index=False)
+
+
+pd.to_timedelta(FORECAST_LENGTH, unit="day")
+
+feature_names = [f"{c} d-{i}" for i in range(HISTORY_LENGTH-1, -1, -1) for c in used_cols]
+feature_importance_df = pd.DataFrame({"feature":feature_names, "importance":mid_model.feature_importances_})
+
+print(feature_importance_df.sort_values("importance", ascending=False).tail(20))
+
--- train.py
+++ train.py
@@ -1,241 +1,245 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
-import numpy as np
+import os
+from pathlib import Path
+
+from sklearn import ensemble, metrics
+import matplotlib.pyplot as plt
+from fast_ml import model_development as md
 import pandas as pd
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.feature_selection import RFE
-from sklearn.linear_model import RidgeCV, LassoCV, Ridge, Lasso
-from sklearn.utils import shuffle
-import matplotlib.pyplot as plt
-import seaborn as sns
-from statsmodels.tsa.stattools import adfuller,acf, pacf
-from statsmodels.tsa.arima_model import ARIMA
-import statsmodels.api as sm
-from pylab import rcParams
+import numpy as np
+import pickle
 
+from model_utils import SaveableModel
 
-# %%
-plt.rcParams['figure.figsize'] = (8, 6)
-plt.rcParams['axes.grid'] = False
-df=pd.read_csv('final_dataset_csv.csv', sep=',',header=0)
-df = df.ffill()
-test_data_size = (int(df.shape[0] * 0.20))
-df.drop(df.tail(test_data_size).index, inplace = True) # remove and set aside 20% of dataset for testing.
+# pathing
+data_dir = Path("data")
+data_file = data_dir / "updated_data_set_full.csv"
+data_dict_file = data_dir / "data_dict.pkl"
+future_data_file = data_dir / "future_dataset.pkl"
 
-TRAIN_SPLIT = int(df.shape[0]*0.70) # 80% training 20 % validation
+model_dir = Path("saved_models")
+model_file = model_dir / "model.pkl"
+ub_model_file = model_dir / "ub_model.pkl"
+lb_model_file = model_dir / "lb_model.pkl"
+
+## Hypers
+SPLITS = {
+    "train":0.70,
+    "validation":0.10,
+    "test":0.20,
+}
+
 # they definitely needs optimization.
-BATCH_SIZE = 32 # bacth size in batch-SGD/variants
-BUFFER_SIZE = 64 # for shuffling the dataset
+BATCH_SIZE = 64 # bacth size in batch-SGD/variants
+BUFFER_SIZE = 10 # for shuffling the dataset
 STEP = 1 # for creation of dataset
 
 # Train and evaluate
-STEPS_PER_EPOCH = 50 # hyperparameter
-EPOCHS = 20 # hyperparameter
-
-
-# %%
-# x = df.drop('RES_LEVEL_FT', axis=1)
-# y = df['RES_LEVEL_FT']
-# X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1, shuffle = False)
-# x.columns
-
-
-# %%
-
-
-
-# %%
-# univariate data
-uni_data_df = df['RES_LEVEL_FT']
-uni_data_df.index = df['FLOW_DATE']
-uni_data_df.head()
-uni_data_df.plot()
-
-
-# %%
-df.head()
-df.columns
-df[['OUTFLOW_CUECS']]
-features = df[['PRESENT_STORAGE_TMC', 'INFLOW_CUSECS', 'OUTFLOW_CUECS', 'tempC', 'windspeedKmph', 'precipMM', 'humidity', 'pressure (mB)', 'cloudcover (%)', 'HeatIndexC', 'DewPointC', 'WindChillC', 'WindGustKmph', 'RES_LEVEL_FT']]
-features.index = df['FLOW_DATE']
-features.head()
-
-
-# %%
-features.plot(subplots = True)
-
-
-# %%
-# standardize the data
-dataset = features.values
-data_mean = dataset[:TRAIN_SPLIT].mean(axis = 0)
-data_std = dataset[:TRAIN_SPLIT].std(axis = 0)
-# data_mean
-data_std.shape
-dataset = (dataset-data_mean)/data_std
-dataset.shape
-
-
-# %%
-data_mean
-
-
-# %%
-def multivariate_data(dataset, target, start_index, end_index, history_size,
-                      target_size, step, single_step=False):
-  data = []
-  labels = []
-
-  start_index = start_index + history_size
-  if end_index is None:
-    end_index = len(dataset) - target_size
-
-  for i in range(start_index, end_index):
-    indices = range(i-history_size, i, step) # step used here.
-    data.append(dataset[indices])
-
-    if single_step: # single_step used here.
-      labels.append(target[i+target_size]) 
-    else:
-      labels.append(target[i:i+target_size]) 
-
-  return np.array(data), np.array(labels)
-
-
-# %%
-dataset.shape
-
-
-# %%
-future_target = 90 # 90 future values
-past_history = 600
-print (dataset[:,13]) # water res levels needs to be predicted
-x_train_multi, y_train_multi = multivariate_data(dataset, dataset[:, 13], 0,
-                                                 TRAIN_SPLIT, past_history,
-                                                 future_target, STEP)
-x_val_multi, y_val_multi = multivariate_data(dataset, dataset[:, 13],
-                                             TRAIN_SPLIT, None, past_history,
-                                             future_target, STEP)
-
-print(x_train_multi.shape)
-print(y_train_multi.shape)
-print(x_val_multi.shape)
-print(y_val_multi.shape)
-
-
-# %%
-# TF DATASET
-
-train_data_multi = tf.data.Dataset.from_tensor_slices((x_train_multi, y_train_multi))
-train_data_multi = train_data_multi.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
-
-val_data_multi = tf.data.Dataset.from_tensor_slices((x_val_multi, y_val_multi))
-val_data_multi = val_data_multi.batch(BATCH_SIZE).repeat()
-
-
-# %%
-np.isnan(y_train_multi).any()
-
-
-# %%
-#utility function
-def create_time_steps(length):
-  return list(range(-length, 0))
-
-print(create_time_steps(20))
-
-
-# %%
-#plotting function
-def multi_step_plot(history, true_future, prediction):
-  plt.figure(figsize=(12, 6))
-  num_in = create_time_steps(len(history))
-  num_out = len(true_future)
-  plt.grid()
-  plt.plot(num_in, np.array(history[:, 1]), label='History')
-  plt.plot(np.arange(num_out)/STEP, np.array(true_future), 'bo',
-           label='True Future')
-  if prediction.any():
-    plt.plot(np.arange(num_out)/STEP, np.array(prediction), 'ro',
-             label='Predicted Future')
-  plt.legend(loc='upper left')
-  plt.show()
-  
-
-
-for x, y in train_data_multi.take(1):
-  multi_step_plot(x[0], y[0], np.array([0]))
-
-
-# %%
-print (x_train_multi.shape[-2:])
-print (x_train_multi.shape)
-# make the LSTM model
-multi_step_model = tf.keras.models.Sequential()
-multi_step_model.add(tf.keras.layers.LSTM(16,
-                                          return_sequences=True,
-                                          input_shape=x_train_multi.shape[-2:]))
-multi_step_model.add(tf.keras.layers.LSTM(8, activation='relu'))
-multi_step_model.add(tf.keras.layers.Dense(90)) # for 90 outputs
-
-# multi_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(clipvalue=1.0), loss='mae')
-# multi_step_model.compile(optimizer=tf.keras.optimizers.Adam(clipvalue=1.0), loss='mae')
-multi_step_model.compile(optimizer=tf.keras.optimizers.Adam(), loss='mae')
-
-
-multi_step_history = multi_step_model.fit(train_data_multi, epochs=EPOCHS,
-                                          steps_per_epoch=STEPS_PER_EPOCH,
-                                          validation_data=val_data_multi,
-                                          validation_steps=50)
-
-
-# %%
-
-
-# Plot train and validation loss over epochs
-
-def plot_train_history(history, title):
-  loss = history.history['loss']
-  val_loss = history.history['val_loss']
-
-  epochs = range(len(loss))
-
-  plt.figure()
-
-  plt.plot(epochs, loss, 'b', label='Training loss')
-  plt.plot(epochs, val_loss, 'r', label='Validation loss')
-  plt.title(title)
-  plt.legend()
-  plt.grid()
-
-  plt.show()
-
-
-# %%
-plot_train_history(multi_step_history, 'Multi-Step Training and validation loss')
-
-
-# %%
-print (multi_step_history)
-
-
-# %%
-for x, y in val_data_multi.take(5):
-  multi_step_plot(x[0], y[0], multi_step_model.predict(x)[0])
-
-
-# %%
-# perform test evaluation
-
-
-# %%
-
-
-
-# %%
-
-
-
+STEPS_PER_EPOCH = 10 # hyperparameter
+EPOCHS = 100 # hyperparameter
+
+FORECAST_LENGTH = 90
+
+HISTORY_LENGTH = 10
+
+
+# data loading
+df = pd.read_csv(data_file, sep=',', na_values=[" ", "&nbsp;"], parse_dates=["FLOW_DATE"], header=0)
+df = df.ffill()
+df = df.set_index("FLOW_DATE")
+df["dummy"] = 1
+
+used_cols = ['PRESENT_STORAGE_TMC', 'INFLOW_CUSECS', 'OUTFLOW_CUECS', 'tempC', 'windspeedKmph', 'precipMM', 'humidity', 'pressure (mB)', 'WindChillC', 'RES_LEVEL_FT', 'OUTFLOW_CUECS_HEMAVATHI', 'OUTFLOW_CUECS_HARANGI']
+
+target_col = 'RES_LEVEL_FT'
+
+def get_windowed_dataset(
+    dataframe,
+    target_col,
+    history_length=HISTORY_LENGTH,
+    forecast_length=FORECAST_LENGTH,
+    batch_size=32,
+    stride=30,
+    ):
+
+    data = dataframe.values.copy()
+    ys = dataframe[target_col]
+
+    data_future = data.copy()
+    data = data[:-forecast_length-history_length]
+    ys = ys[forecast_length+history_length:] # predict window starting at next time step
+
+    # window the labels to make forecasts in the future
+    windowed_ys = tf.keras.preprocessing.timeseries_dataset_from_array(
+        data=ys,
+        targets=None, # ys.index,
+        sequence_length=forecast_length,
+        sequence_stride=stride,
+        shuffle=False,
+        batch_size=1,
+    )
+    ys = np.array(list(windowed_ys.as_numpy_iterator())).squeeze()
+    dataset = tf.keras.preprocessing.timeseries_dataset_from_array(
+        data=data,
+        targets=ys,
+        sequence_length=history_length,
+        sequence_stride=stride,
+        shuffle=False,
+        batch_size=batch_size,
+    )
+    future_dataset = tf.keras.preprocessing.timeseries_dataset_from_array(
+        data=data_future,
+        targets=np.ones((data_future.shape[0],1)),
+        sequence_length=history_length,
+        sequence_stride=stride,
+        shuffle=False,
+        batch_size=batch_size,
+    )
+    return dataset, future_dataset
+
+def dataset_to_arrays(dataset, flatten=True, last_y=True):
+    all_xs = []
+    all_ys = []
+    for x, y in dataset.as_numpy_iterator():
+        all_xs.append(x)
+        all_ys.append(y)
+    ys = np.concatenate(all_ys)
+    xs = np.concatenate(all_xs)
+    if flatten:
+        xs = xs.reshape(xs.shape[0], -1)
+    if last_y:
+        ys = ys[:, -1]
+    return xs, ys
+
+historical_dataset, future_dataset  = get_windowed_dataset(
+    df[used_cols],# + ["dummy"]],
+    target_col=target_col,
+    stride=1
+    )
+
+historical_xs, historical_ys = dataset_to_arrays(historical_dataset)
+
+# series of y's which are indexed by date
+historical_ys_series = df[target_col].iloc[-historical_ys.shape[0]:]
+assert (historical_ys_series == historical_ys).all(), "something went wrong with historical y indexes"
+
+future_xs, _ = dataset_to_arrays(future_dataset)
+
+with open(future_data_file, "wb") as f:
+    pickle.dump(future_xs, f)
+
+data_df = pd.DataFrame(np.concatenate([np.arange(historical_ys.shape[0])[:, None], historical_xs, historical_ys[:,None]], axis=-1))
+
+data_df.index = historical_ys_series.index
+
+X_train, y_train, X_valid, y_valid, X_test, y_test = md.train_valid_test_split(
+    data_df,
+    target=data_df.columns[-1], # use dummy for train test split before preproccessing
+    train_size=SPLITS["train"],
+    valid_size=SPLITS["validation"],
+    test_size=SPLITS["test"],
+    method="sorted",
+    sort_by_col=data_df.columns[0],
+)
+
+# use .iloc[:,1:]to drop first column
+# the dataframes are indexed by the date of the y value (the future flow)
+data_dict = {
+    "train": {"X":X_train.iloc[:,1:], "y":y_train, "dates": y_train.index},
+    "validation": {"X":X_valid.iloc[:,1:], "y":y_valid, "dates": y_valid.index},
+    "test": {"X":X_test.iloc[:,1:], "y":y_test, "dates": y_test.index},
+}
+
+with open(data_dict_file, "wb") as f:
+    pickle.dump(data_dict, f)
+
+X_means = data_dict["train"]["X"].mean()
+X_stdev = data_dict["train"]["X"].std()
+y_means = data_dict["train"]["y"].mean()
+y_stdev = data_dict["train"]["y"].std()
+for partition in data_dict.keys():
+    data_dict[partition]["X"] = (data_dict[partition]["X"] - X_means) / X_stdev
+    data_dict[partition]["y"] = (data_dict[partition]["y"] - y_means) / y_stdev
+
+future_xs = (future_xs - X_means[None,]) / X_stdev[None,:]
+
+# climate that gets the most rainfall, months of may to october get most rainfall
+monsoon_weight = 20
+for part, d in data_dict.items():
+    data_dict[part]["is_monsoon_month"] = (5 <= d["dates"].month) & (d["dates"].month <= 10)
+    data_dict[part]["sample_weight"] = np.ones_like(data_dict[part]["is_monsoon_month"]).astype(float)
+    data_dict[part]["sample_weight"][data_dict[part]["is_monsoon_month"]] = monsoon_weight
+
+kwargs = dict(
+    max_depth=20,
+    subsample=0.5, n_estimators = 200
+)
+
+lower_model = ensemble.GradientBoostingRegressor(loss="quantile",alpha=0.1,**kwargs,)
+mid_model = ensemble.GradientBoostingRegressor(loss="ls", **kwargs)
+upper_model = ensemble.GradientBoostingRegressor(loss="quantile", alpha=0.9, **kwargs)
+
+train_args = {
+    "X":data_dict["train"]['X'],
+    "y":data_dict["train"]["y"],
+    "sample_weight":data_dict["train"]["sample_weight"]
+}
+lower_model.fit(**train_args)
+mid_model.fit(**train_args)
+upper_model.fit(**train_args)
+
+model = SaveableModel(y_means, y_stdev, mid_model)
+lb_model = SaveableModel(y_means, y_stdev, lower_model)
+ub_model = SaveableModel(y_means, y_stdev, upper_model)
+
+
+result_dict = {}
+for partition, d in data_dict.items():
+    rescale_y_pred = model.predict(d["X"])
+    rescale_y_true = model.rescale(d["y"])
+    mae = metrics.mean_absolute_error(rescale_y_true, rescale_y_pred)
+    monsoon_mae = metrics.mean_absolute_error(rescale_y_true[d["is_monsoon_month"]], rescale_y_pred[d["is_monsoon_month"]])
+    non_monsoon_mae = metrics.mean_absolute_error(rescale_y_true[~d["is_monsoon_month"]], rescale_y_pred[~d["is_monsoon_month"]])
+    result_dict[partition] = {
+        "mae":mae,
+        "monsoon_mae": monsoon_mae,
+        "non_monsoon_mae": non_monsoon_mae
+    }
+    print(f"{partition} mae:", mae)
+    print(f"{partition} monsoon mae:", monsoon_mae)
+    print(f"{partition} non monsoon mae:", non_monsoon_mae)
+    # multi_step_plot(xs[:, -1], ys, y_pred, title=f"{partition}")
+
+print("reslts")
+print(pd.DataFrame(result_dict))
+
+# save predictive model, i.e. y_stdev, y_means, lower_model, mid_model, upper_model
+
+historical_xs.shape, X_means.shape
+
+# refit model on full historical dataset and predict on future dataset
+
+historical_training_xs = (historical_xs - X_means[None,]) / X_stdev[None,]
+historical_training_ys = (historical_ys - y_means) / y_stdev
+
+lower_model = ensemble.GradientBoostingRegressor(loss="quantile",alpha=0.1,**kwargs,)
+mid_model = ensemble.GradientBoostingRegressor(loss="ls", **kwargs)
+upper_model = ensemble.GradientBoostingRegressor(loss="quantile", alpha=0.9, **kwargs)
+
+lower_model.fit(historical_training_xs, historical_training_ys)
+mid_model.fit(historical_training_xs, historical_training_ys)
+upper_model.fit(historical_training_xs, historical_training_ys)
+
+model = SaveableModel(y_means, y_stdev, mid_model)
+lb_model = SaveableModel(y_means, y_stdev, lower_model)
+ub_model = SaveableModel(y_means, y_stdev, upper_model)
+
+print("training on full historical dataset and saving models to file")
+with open(model_file, "wb") as f:
+    pickle.dump(model, f)
+
+with open(lb_model_file, "wb") as f:
+    pickle.dump(lb_model, f)
+
+with open(ub_model_file, "wb") as f:
+    pickle.dump(ub_model, f)
